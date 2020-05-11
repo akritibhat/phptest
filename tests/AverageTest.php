60b41bf11b62f4b5d36cba80e3fd675b3655815d
Akriti Bhat <8200097+akritibhat@users.noreply.github.com>
2020-05-10 17:00:44 -0700

    Update AverageTest.php

--- tests/AverageTest.php
+++ tests/AverageTest.php
@@ -36,3 +36,25 @@ class AverageTest extends TestCase
         $this->assertEquals(5, $this->Average->median($numbers));
     }
 }
+
+class AverageTest2 extends TestCase
+{
+    protected $Average2;
+
+    public function setUp()
+    {
+        $this->Average2 = new Average();
+    }
+
+    public function testCalculationOfMean2()
+    {
+        $numbers = [3, 7, 6, 1, 5];
+        $this->assertEquals(4.4, $this->Average2->mean($numbers));
+    }
+
+    public function testCalculationOfMedian2()
+    {
+        $numbers = [3, 7, 6, 1, 5];
+        $this->assertEquals(5, $this->Average2->median($numbers));
+    }
+}
