a46ecea75c3751c0be5e0256939c06b55e22651d
Akriti Bhat <8200097+akritibhat@users.noreply.github.com>
2020-05-19 20:49:09 -0700

    Update testingfile2.php

--- tests2/testingfile2.php
+++ tests2/testingfile2.php
@@ -1,27 +1,12 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
-final class EmailTest extends TestCase
+final class EmailTest2 extends TestCase
 {
-    public function testCanBeCreatedFromValidEmailAddress(): void
+    public function testCanBeCreatedFromValidEmailAddress2(): void
     {
         $this->assertInstanceOf(
             Email::class,
-            Email::fromString('user@example.com')
-        );
-    }
-
-    public function testCannotBeCreatedFromInvalidEmailAddress(): void
-    {
-        $this->expectException(InvalidArgumentException::class);
-
-        Email::fromString('invalid');
-    }
-
-    public function testCanBeUsedAsString(): void
-    {
-        $this->assertEquals(
-            'user@example.com',
             Email::fromString('user@example.com')
         );
     }
